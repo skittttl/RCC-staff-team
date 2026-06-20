01f8241184c75018da3a6075bb23f949c354cc40
staff <skittttll@gmail.com>
2026-06-20 18:30:01 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -26,7 +26,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "Managing the server",
         // Activity type number (0 = Playing).
-        type: 2,
+        type: 1,
       },
     ],
   },
