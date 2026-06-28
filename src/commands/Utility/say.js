e78a6819b8b9296d1a9c26f254b7f63506778d1a
staff <skittttll@gmail.com>
2026-06-29 01:22:13 +0200

    Update say.js

--- src/commands/Utility/say.js
+++ src/commands/Utility/say.js
@@ -1,1 +1,25 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('say')
+        .setDescription('Send a normal message')
+        .addStringOption(option =>
+            option
+                .setName('message')
+                .setDescription('The message to send')
+                .setRequired(true)
+        )
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
+
+    async execute(interaction) {
+        const message = interaction.options.getString('message');
+
+        await interaction.reply({
+            content: 'Message sent.',
+            ephemeral: true
+        });
+
+        await interaction.channel.send(message);
+    }
+};
